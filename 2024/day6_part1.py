e94e243869e7cd3898dcecba40d2748006ddf1c4
abhishek <abhishek20rajput@gmail.com>
2024-12-06 17:38:50 +0530

    better runtime solutions for day6

--- 2024/day6_part1.py
+++ 2024/day6_part1.py
@@ -27,27 +27,32 @@ def solve():
         
         return x, y
 
-    res = set()
     n, m = len(lab_map), len(lab_map[0])
-    
+
+    def traverse(start_x, start_y):
+        x, y = start_x, start_y
+        visited = [[0] * m for _ in range(n)]
+        direction = 0
+
+        while 0 <= x < n and 0 <= y < m:
+            visited[x][y] = 1
+            nx, ny = move(x, y, direction)
+
+            if 0 <= nx < n and 0 <= ny < m and lab_map[nx][ny] == '#':
+                direction = (direction + 1) % 4
+            else:
+                x, y = nx, ny
+
+        return visited
+
     for i in range(n):
         for j in range(m):
             if lab_map[i][j] == '^':
-                x, y = i, j
+                start_x, start_y = i, j
                 break
-
-    direction = 0
-
-    while 0 <= x < n and 0 <= y < m:
-        res.add((x, y))
-        nx, ny = move(x, y, direction)
-
-        if 0 <= nx < n and 0 <= ny < m and lab_map[nx][ny] == '#':
-            direction = (direction + 1) % 4
-        else:
-            x, y = nx, ny
-
-    return len(res)
+    
+    visited = traverse(start_x, start_y)
+    return sum(sum(row) for row in visited)
 
 
 print(solve())
--- 2024/day6_part2.py
+++ 2024/day6_part2.py
@@ -32,23 +32,22 @@ def solve():
 
     def traverse(start_x, start_y, obstacle_x = -1, obstacle_y = -1):
         x, y = start_x, start_y
-        res = set()
+        res = [[0] * m for _ in range(n)]
         seen = set()
         direction = 0
 
         while 0 <= x < n and 0 <= y < m:
-            res.add((x, y))
-            seen.add((x, y, direction))
+            res[x][y] = 1
             nx, ny = move(x, y, direction)
 
             if 0 <= nx < n and 0 <= ny < m and (lab_map[nx][ny] == '#' or (nx == obstacle_x and ny == obstacle_y)):
                 direction = (direction + 1) % 4
+                if obstacle_x != -1 and obstacle_y != -1 and (x, y, direction) in seen:
+                    return 1, 0
+                seen.add((x, y, direction))
             else:
                 x, y = nx, ny
 
-            if obstacle_x != -1 and obstacle_y != -1 and (x, y, direction) in seen:
-                return 1, 0
-        
         return 0, res
 
     for i in range(n):
@@ -59,9 +58,10 @@ def solve():
     
     visited = traverse(start_x, start_y)[1]
     
-    for x, y in visited:
-        if traverse(start_x, start_y, x, y)[0]:
-            res += 1
+    for x in range(n):
+        for y in range(m):
+            if visited[x][y] and traverse(start_x, start_y, x, y)[0]:
+                res += 1
         
     return res
 
